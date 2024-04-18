77918cc50f0e8b59643c51f68c7a7970b625a357
elspycongry <vuthanhtungcv1@gmail.com>
2024-04-19 04:14:44 +0700

    fix error network

--- tiktok-app/src/pages/Upload/upload.js
+++ tiktok-app/src/pages/Upload/upload.js
@@ -1,5 +1,40 @@
+import axios, {put, putForm} from "axios";
+import Button from "../../component/Button";
+import {useState} from "react";
+
 function Upload() {
-    return <h2>Upload page</h2>
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+        axios.post('http://localhost:8080/files', formData)
+            .then(response => {
+                // console.log('File upload successful:', response.data);
+                alert("ok")
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    };
+
+    return (
+        <div>
+            <h2>Upload Page</h2>
+
+            <form onSubmit={handleSubmit} encType={"multipart/form-data "}>
+                <input type="file" onChange={handleFileChange} />
+                <Button type="submit" primary  >
+                    Upload
+                </Button>
+            </form>
+        </div>
+    );
 }
 
 export default Upload;
